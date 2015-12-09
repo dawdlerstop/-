087cb2de2ad9ee3a84fa1456f86bb7e224636cce
liuyujing <liuyujingmusic@163.com>
2015-12-09 16:08:49 +0800

    Initial Commit

---
+++ 代理实战/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  代理实战
+//
+//  Created by Bruce on 15/12/9.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 代理实战/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  代理实战
+//
+//  Created by Bruce on 15/12/9.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
